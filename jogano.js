3a3f4fc138cfeb572483b2523ab90dc3a23c29cb
Isabella Wang <isabella@Isabellas-MacBook.local>
2018-08-06 07:00:10 +0800

    modified:   1-products/.DS_Store modified:   jogano.css modified:   jogano.html modified:   jogano.js

---
+++ jogano.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+$( ".navi2" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".navi2" ).slideToggle( function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".navi2" ).slideToggle( function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+});
