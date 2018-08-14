915f1000833160c9ff07f4adaf7848ca2932ea2d
Isabella Wang <isabella@Isabellas-MacBook.local>
2018-08-14 11:06:58 +0800

    modified:   jogano.css modified:   jogano.js modified:   product.html

--- jogano.js
+++ jogano.js
@@ -16,4 +16,18 @@ $( ".hamburger" ).show();
 });
 });
 
+
+$('input').click(function() {
+    var selector = $('input:checkbox').map(function(){ 
+        return this.checked ? '.' + this.id : ''; 
+    }).get().join('');
+    console.log(selector);  
+
+    var all = $('div[class^="category"]');
+    if(selector.length)
+      all.hide().filter(selector).show()
+    else all.show();
+});
+
+
 });
