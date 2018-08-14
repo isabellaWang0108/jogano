673e134a2cd07b0455c44af1cc015556e853426b
Isabella Wang <isabella@Isabellas-MacBook.local>
2018-08-14 13:56:50 +0800

    modified:   .DS_Store modified:   jogano.js modified:   product.html renamed:    product/.DS_Store -> projects/.DS_Store renamed:    product/1.html -> projects/1.html renamed:    product/6.html -> projects/6.html renamed:    product/product.css -> projects/jogano.css

--- jogano.js
+++ jogano.js
@@ -18,16 +18,13 @@ $( ".hamburger" ).show();
 
 
 $('input').click(function() {
-    var selector = $('input:checkbox').map(function(){ 
-        return this.checked ? '.' + this.id : ''; 
-    }).get().join('');
-    console.log(selector);  
+    var category = $(this).val();
 
-    var all = $('div[class^="category"]');
-    if(selector.length)
-      all.hide().filter(selector).show()
-    else all.show();
+    $(this).attr('checked') $ except('.' + category).hide();
+    
 });
 
 
 });
+
+
