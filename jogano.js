806ada5f36a2bf1ea595b707b2ac7bcff7aedb93
Isabella Wang <isabella@Isabellas-MacBook.local>
2018-08-21 14:31:29 +0800

    modified:   jogano.css modified:   jogano.js modified:   product/.DS_Store modified:   product/1.html new file:   product/10.html new file:   product/11.html new file:   product/13.html new file:   product/14.html new file:   product/18.html new file:   product/19.html renamed:    product/6.html -> product/2.html new file:   product/20.html new file:   product/21.html new file:   product/22.html new file:   product/23.html new file:   product/24.html new file:   product/29.html new file:   product/30.html new file:   product/31.html new file:   product/4.html new file:   product/47.html new file:   product/7.html new file:   product/8.html new file:   product/9.html modified:   products.html

--- jogano.js
+++ jogano.js
@@ -32,25 +32,30 @@ $( ".apply" ).show();
 });
 });
 
+//data-filter-view属性値を操作する処理
+//DOM構造を組み替えずにCSSによる描画処理の力で要素の出しわけが行える
 var widget = document.getElementById('filterContent');
 var checkboxes = widget.querySelectorAll('input[type="checkbox"]');
 var checkedList = [];
+//チェックボックスの値が変更されるタイミングで、filter関数が呼び出し
 var filter = function () {
     checkedList = [];
 
+  //選択されているチェックボックスのvalue属性値を配列に格納します
     Array.prototype.forEach.call(checkboxes, function (input) {
         if (input.checked) {
             checkedList.push(input.value);
         }
     });
 
-
+  //ルートの要素に、配列を半角スペースで.join()した値を持つdata-filter-view属性をセット
     widget.setAttribute('data-filter-view', checkedList.join(' '));
 };
 
 Array.prototype.forEach.call(checkboxes, function (checkbox) {
     checkbox.addEventListener('change', filter);
 });
+
 
 
 
