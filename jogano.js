08808f00ffc882b6ea3cf844632a4e5afdb4a88a
Isabella Wang <isabella@Isabellas-MacBook.local>
2018-08-22 10:21:50 +0800

    modified:   jogano.js

--- jogano.js
+++ jogano.js
@@ -32,157 +32,129 @@ $( ".apply" ).show();
 });
 });
 
-});
+(function(){
+  'use strict'
+  angular.module('angularDemo', ['ngMaterial']); //initialize module
 
+  //can reference model instead of creating a global variable
+  angular.module('angularDemo').controller('angularController', 
+                                           ['$scope','ProductDataService', function($scope, ProductDataService) { 
+    var products = ProductDataService.getSampleData();
+    $scope.Fruits = products; //use $scope to expose to the view
 
+    //create checkbox filters on the fly with dynamic data
+    var filters = [];
+    _.each(products, function(product) {
+      _.each(product.properties, function(property) {      
+        var existingFilter = _.findWhere(filters, { name: property.name });
 
-var checkboxFilter = {
-  
-  // Declare any variables we will need as properties of the object
-  
-  $filters: null,
-  $reset: null,
-  groups: [],
-  outputArray: [],
-  outputString: '',
-  
-  // The "init" method will run on document ready and cache any jQuery objects we will need.
-  
-  init: function(){
-    var self = this; // As a best practice, in each method we will asign "this" to the variable "self" so that it remains scope-agnostic. We will use it to refer to the parent "checkboxFilter" object so that we can share methods and properties between all parts of the object.
-    
-    self.$filters = $('#Filters');
-    self.$reset = $('#Reset');
-    self.$container = $('#Container');
-    
-    self.$filters.find('fieldset').each(function(){
-      self.groups.push({
-        $inputs: $(this).find('input'),
-        active: [],
-		    tracker: false
-      });
-    });
-    
-    self.bindHandlers();
-  },
-  
-  // The "bindHandlers" method will listen for whenever a form value changes. 
-  
-  bindHandlers: function(){
-    var self = this;
-    
-    self.$filters.on('change', function(){
-      self.parseFilters();
-    });
-    
-    self.$reset.on('click', function(e){
-      e.preventDefault();
-      self.$filters[0].reset();
-      self.parseFilters();
-    });
-  },
-  
-  // The parseFilters method checks which filters are active in each group:
-  
-  parseFilters: function(){
-    var self = this;
- 
-    // loop through each filter group and add active filters to arrays
-    
-    for(var i = 0, group; group = self.groups[i]; i++){
-      group.active = []; // reset arrays
-      group.$inputs.each(function(){ 
-        $(this).is(':checked') && group.active.push(this.value);
-      });
-	    group.active.length && (group.tracker = 0);
-    }
-    
-    self.concatenate();
-  },
-  
-  // The "concatenate" method will crawl through each group, concatenating filters as desired:
-  
-  concatenate: function(){
-    var self = this,
-		  cache = '',
-		  crawled = false,
-		  checkTrackers = function(){
-        var done = 0;
-        
-        for(var i = 0, group; group = self.groups[i]; i++){
-          (group.tracker === false) && done++;
-        }
-
-        return (done < self.groups.length);
-      },
-      crawl = function(){
-        for(var i = 0, group; group = self.groups[i]; i++){
-          group.active[group.tracker] && (cache += group.active[group.tracker]);
-
-          if(i === self.groups.length - 1){
-            self.outputArray.push(cache);
-            cache = '';
-            updateTrackers();
-          }
-        }
-      },
-      updateTrackers = function(){
-        for(var i = self.groups.length - 1; i > -1; i--){
-          var group = self.groups[i];
-
-          if(group.active[group.tracker + 1]){
-            group.tracker++; 
-            break;
-          } else if(i > 0){
-            group.tracker && (group.tracker = 0);
+        if (existingFilter) {
+          var existingOption = _.findWhere(existingFilter.options, { value: property.value });
+          if (existingOption) {
+            existingOption.count += 1;
           } else {
-            crawled = true;
+            existingFilter.options.push({ value: property.value, count: 1 }); 
+          }   
+        } else {
+          var filter = {};
+          filter.name = property.name;
+          filter.options = [];
+          filter.options.push({ value: property.value, count: 1 });
+          filters.push(filter);      
+        }   
+      });
+    });
+    $scope.Filters = filters;
+
+    this.toggleAll = function($event, includeAll) {       
+      _.each(filters, function(filterCategory) {      
+        _.each(filterCategory.options, function(option) {
+          option.IsIncluded = includeAll;
+        });
+      });    
+    };
+  }]);
+
+  angular.module('angularDemo').filter('dynamicFilter', function () {
+    return function (products, filterCategories, scope) {
+      var filtered = [];
+
+      var productFilters = _.filter(filterCategories, function(fc) {
+        return  _.any(fc.options, { 'IsIncluded': true });
+      });
+
+      _.each(products, function(prod) {
+        var includeProduct = true;
+        _.each(productFilters, function(filter) {
+          var props = _.filter(prod.properties, { 'name': filter.name });
+          if (!_.any(props, function(prop) { return _.any(filter.options, { 'value': prop.value, 'IsIncluded': true }); })) {
+            includeProduct = false;
           }
+        });
+        if (includeProduct) {
+          filtered.push(prod);
         }
-      };
-    
-    self.outputArray = []; // reset output array
+      });
+      return filtered;
+    };
+  });
 
-	  do{
-		  crawl();
-	  }
-	  while(!crawled && checkTrackers());
+  angular.module('angularDemo').service('ProductDataService', function() {
+    var service = {};
 
-    self.outputString = self.outputArray.join();
-    
-    // If the output string is empty, show all rather than none:
-    
-    !self.outputString.length && (self.outputString = 'all'); 
-    
-    //console.log(self.outputString); 
-    
-    // ^ we can check the console here to take a look at the filter string that is produced
-    
-    // Send the output string to MixItUp via the 'filter' method:
-    
-	  if(self.$container.mixItUp('isLoaded')){
-    	self.$container.mixItUp('filter', self.outputString);
-	  }
-  }
-};
-  
-// On document ready, initialise our code.
+    //sample data
+    var products = [
+      {
+        name: 'apple',
+        properties: [
+          { name:'type', value:'fruit' }, { name:'color', value:'red' }, 
+          { name:'size', value:'medium' }, { name:'shape', value:'weird' }
+        ]
+      },{
+        name: 'orange',
+        properties: [
+          { name:'type', value:'fruit' }, { name:'color', value:'orange'},
+          { name:'size', value:'medium' }, { name:'shape', value:'sphere'}
+        ]
+      },{
+        name: 'grapefruit',
+        properties: [
+          { name:'type', value:'fruit' }, { name:'color', value:'yellow' },
+          { name:'size', value:'large' }, { name:'shape', value:'sphere' }
+        ]
+      },{
+        name: 'lemon',
+        properties: [
+          { name:'type', value:'fruit' }, { name:'color', value:'yellow' },
+          { name:'size', value:'small' }, { name:'shape', value:'lemon' }
+        ]
+      },{
+        name: 'lime',
+        properties: [
+          { name:'type', value:'fruit' }, { name: 'color', value: 'green' },
+          { name:'size', value:'small' }, { name: 'shape', value: 'lemon' }
+        ]
+      },{
+        name:'pepper',
+        properties: [
+          { name:'type', value:'vegetable' }, { name:'color', value:'red' },
+          { name:'size', value:'medium' }, { name:'shape', value:'weird' }
+        ]
+      }    
+    ];
 
-$(function(){
-      
-  // Initialize checkboxFilter code
-      
-  checkboxFilter.init();
-      
-  // Instantiate MixItUp
-      
-  $('#Container').mixItUp({
-    controls: {
-      enable: false // we won't be needing these
-    },
-    animation: {
-      easing: 'cubic-bezier(0.86, 0, 0.07, 1)',
-      duration: 600
-    }
-  });    
+    service.getSampleData = function() {
+      return products;
+    };
+
+    return service;
+  });
+
+})();
+
+
+
+
 });
+
+
