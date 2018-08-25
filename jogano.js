7c9072fd9bbd79dbd2eb7ad4992e969ee499fb47
Isabella Wang <isabella@Isabellas-MacBook.local>
2018-08-25 07:08:43 -0400

    new file:   chinese/.DS_Store new file:   chinese/About.html new file:   chinese/events.html new file:   chinese/home.html new file:   chinese/product/.DS_Store new file:   chinese/product/1.html new file:   chinese/product/10.html new file:   chinese/product/11.html new file:   chinese/product/13.html new file:   chinese/product/14.html new file:   chinese/product/18.html new file:   chinese/product/19.html new file:   chinese/product/2.html new file:   chinese/product/20.html new file:   chinese/product/21.html new file:   chinese/product/22.html new file:   chinese/product/23.html new file:   chinese/product/24.html new file:   chinese/product/29.html new file:   chinese/product/30.html new file:   chinese/product/31.html new file:   chinese/product/4.html new file:   chinese/product/47.html new file:   chinese/product/7.html new file:   chinese/product/8.html new file:   chinese/product/9.html new file:   chinese/product/jogano.css new file:   chinese/products.html modified:   home.html modified:   jogano.js modified:   test.css modified:   test.html

--- jogano.js
+++ jogano.js
@@ -32,125 +32,31 @@ $( ".apply" ).show();
 });
 });
 
-(function(){
-  'use strict'
-  angular.module('angularDemo', ['ngMaterial']); //initialize module
+//data-filter-view属性値を操作する処理
+//DOM構造を組み替えずにCSSによる描画処理の力で要素の出しわけが行える
+var widget = document.getElementById('filterContent');
+var checkboxes = widget.querySelectorAll('input[type="checkbox"]');
+var checkedList = [];
+//チェックボックスの値が変更されるタイミングで、filter関数が呼び出し
+var filter = function () {
+    checkedList = [];
 
-  //can reference model instead of creating a global variable
-  angular.module('angularDemo').controller('angularController', 
-                                           ['$scope','ProductDataService', function($scope, ProductDataService) { 
-    var products = ProductDataService.getSampleData();
-    $scope.Fruits = products; //use $scope to expose to the view
-
-    //create checkbox filters on the fly with dynamic data
-    var filters = [];
-    _.each(products, function(product) {
-      _.each(product.properties, function(property) {      
-        var existingFilter = _.findWhere(filters, { name: property.name });
-
-        if (existingFilter) {
-          var existingOption = _.findWhere(existingFilter.options, { value: property.value });
-          if (existingOption) {
-            existingOption.count += 1;
-          } else {
-            existingFilter.options.push({ value: property.value, count: 1 }); 
-          }   
-        } else {
-          var filter = {};
-          filter.name = property.name;
-          filter.options = [];
-          filter.options.push({ value: property.value, count: 1 });
-          filters.push(filter);      
-        }   
-      });
-    });
-    $scope.Filters = filters;
-
-    this.toggleAll = function($event, includeAll) {       
-      _.each(filters, function(filterCategory) {      
-        _.each(filterCategory.options, function(option) {
-          option.IsIncluded = includeAll;
-        });
-      });    
-    };
-  }]);
-
-  angular.module('angularDemo').filter('dynamicFilter', function () {
-    return function (products, filterCategories, scope) {
-      var filtered = [];
-
-      var productFilters = _.filter(filterCategories, function(fc) {
-        return  _.any(fc.options, { 'IsIncluded': true });
-      });
-
-      _.each(products, function(prod) {
-        var includeProduct = true;
-        _.each(productFilters, function(filter) {
-          var props = _.filter(prod.properties, { 'name': filter.name });
-          if (!_.any(props, function(prop) { return _.any(filter.options, { 'value': prop.value, 'IsIncluded': true }); })) {
-            includeProduct = false;
-          }
-        });
-        if (includeProduct) {
-          filtered.push(prod);
+  //選択されているチェックボックスのvalue属性値を配列に格納します
+    Array.prototype.forEach.call(checkboxes, function (input) {
+        if (input.checked) {
+            checkedList.push(input.value);
         }
-      });
-      return filtered;
-    };
-  });
+    });
 
-  angular.module('angularDemo').service('ProductDataService', function() {
-    var service = {};
+  //ルートの要素に、配列を半角スペースで.join()した値を持つdata-filter-view属性をセット
+    widget.setAttribute('data-filter-view', checkedList.join(' '));
+};
 
-    //sample data
-    var products = [
-      {
-        name: 'apple',
-        properties: [
-          { name:'type', value:'fruit' }, { name:'color', value:'red' }, 
-          { name:'size', value:'medium' }, { name:'shape', value:'weird' }
-        ]
-      },{
-        name: 'orange',
-        properties: [
-          { name:'type', value:'fruit' }, { name:'color', value:'orange'},
-          { name:'size', value:'medium' }, { name:'shape', value:'sphere'}
-        ]
-      },{
-        name: 'grapefruit',
-        properties: [
-          { name:'type', value:'fruit' }, { name:'color', value:'yellow' },
-          { name:'size', value:'large' }, { name:'shape', value:'sphere' }
-        ]
-      },{
-        name: 'lemon',
-        properties: [
-          { name:'type', value:'fruit' }, { name:'color', value:'yellow' },
-          { name:'size', value:'small' }, { name:'shape', value:'lemon' }
-        ]
-      },{
-        name: 'lime',
-        properties: [
-          { name:'type', value:'fruit' }, { name: 'color', value: 'green' },
-          { name:'size', value:'small' }, { name: 'shape', value: 'lemon' }
-        ]
-      },{
-        name:'pepper',
-        properties: [
-          { name:'type', value:'vegetable' }, { name:'color', value:'red' },
-          { name:'size', value:'medium' }, { name:'shape', value:'weird' }
-        ]
-      }    
-    ];
+Array.prototype.forEach.call(checkboxes, function (checkbox) {
+    checkbox.addEventListener('change', filter);
+});
 
-    service.getSampleData = function() {
-      return products;
-    };
 
-    return service;
-  });
-
-})();
 
 
 
